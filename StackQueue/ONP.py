649e99439d5f09e614b401f5fda946fce56996a9
binhle <chibinh8@gmail.com>
2017-07-10 23:42:34 +0700

    ONP commit

---
+++ StackQueue/ONP.py
@@ -0,0 +1,58 @@
+__author__ = 'USER'
+
+def EvaluateInput(Arg):
+    if(Arg==[]):
+        return 0
+    if(list(Arg).count(" ")!=0):
+        return 0
+    return 1
+def Handle(Arg):
+   for ChildList in Arg:
+       if(EvaluateInput(ChildList)==1):
+           Handle4OneInput(ChildList)
+
+   return 1
+def IsVal(Arg):
+    if((Arg=='(')or(Arg==')')or(Arg=='+')or(Arg=='-')or(Arg=='*')or(Arg=='/')or(Arg=='^')):
+        return 0
+    return 1
+def IsOperator(Arg):
+    if((Arg=='+')or(Arg=='-')or(Arg=='*')or(Arg=='/')or(Arg=='^')):
+        return 1
+    return 0
+def Handle4OneInput(Arg):
+    StackVal = []
+    StackOper =[]
+    ListPrint = []
+    for c in Arg:
+        if(IsVal(c)==1):
+            StackVal.append(c)
+        elif(IsOperator(c)==1):
+            StackOper.append(c)
+        elif(c==")"):
+            a = ""
+            b = ""
+            if(len(StackVal)>=1):
+             a = StackVal.pop()
+             if(len(StackVal)>=1):
+               b = StackVal.pop()
+            if(len(StackOper)!=0):
+                if(a!=""):
+                    ListPrint.append(a)
+                if(b!=""):
+                    ListPrint.append(a)
+                a=""
+                b=""
+                ListPrint.append(StackOper.pop())
+    print(ListPrint)
+
+    return 0
+
+if __name__ == '__main__':
+    Arg = []
+    N = int(raw_input().split()[0])
+    while(N>0):
+        Arg.append(raw_input())
+        N -=1
+    print(Handle(Arg))
+    pass
