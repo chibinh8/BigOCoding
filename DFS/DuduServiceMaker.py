8bddb6100c71f54d85e4ef9ee45072b5b7093053
binhle <chibinh8@gmail.com>
2017-07-24 15:50:23 +0700

    DFS: BTVN1

--- DFS/DuduServiceMaker.py
+++ DFS/DuduServiceMaker.py
@@ -12,6 +12,7 @@ def Handle(Arg, Arg1):
    Graph = []
    N = Arg1[1]
    DepenCnt = 0
+   PrevDepenCnt =0
    for c in range(0,Arg1[0]):
        Path.append(-1)
        Visited.append(False)
@@ -19,23 +20,46 @@ def Handle(Arg, Arg1):
    for c in range(0,N):
         Graph[int(Arg[c][0])-1].append(int(Arg[c][1]))
    for cd in range(1,Arg1[0]+1) :
-       StackRun.append(cd)
-       ParentNode = cd
-       while(len(StackRun)>0):
-            PopL = StackRun.pop()
-            ParentNode = int(PopL)
-            ListC = list(Graph[int(ParentNode)-1])
-            for Ele in ListC:
-              if(Visited[int(Ele)-1]==False):
-                  Visited[int(Ele)-1]=True
-                  Path[int(Ele)-1] = ParentNode
-                  StackRun.append(Ele)
-                  DepenCnt +=1
+       if(Graph[cd-1]!=[]):
+           StackRun.append(cd)
+           ParentNode = cd
+           while(len(StackRun)>0):
+                PopL = StackRun.pop()
+                ParentNode = int(PopL)
+                #print(ParentNode)
+                ListC = list(Graph[int(ParentNode)-1])
+                if(Visited[ParentNode-1]==True):
+                    Indicate = False
+                    for dc in ListC:
+                        if(Visited[dc-1]==False):
+                          Indicate = True
+                    if(Indicate==False):
+                      DepenCnt = PrevDepenCnt
+                      if(DepenCnt>1):
+                          print(DepenCnt)
+                          return "SIM"
+                else:
+                    if(ListC!=[]):
+                        DepenCnt +=1
+                        PrevDepenCnt = DepenCnt
+                        Visited[ParentNode-1]=True
+                    else:
+                        DepenCnt = PrevDepenCnt
+                        if(DepenCnt>1):
+                               print(DepenCnt)
+                               return "SIM"
+                for Ele in ListC:
+                  if(Visited[int(Ele)-1]==False):
+                      StackRun.append(Ele)
+
        if(DepenCnt>1):
+           print(DepenCnt)
            return "SIM"
        DepenCnt = 0
        StackRun = []
-       Visited = []
+       PrevDepenCnt =0
+       for c in range(0,Arg1[0]):
+           Visited[c] = False
    return  "NAO"
 
 if __name__ == '__main__':
