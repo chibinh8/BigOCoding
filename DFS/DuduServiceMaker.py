f6d504531c3d5e824bceb7ac498739f9b72f6852
binhle <chibinh8@gmail.com>
2017-07-25 23:40:47 +0700

    DFS: BTVN1

--- DFS/DuduServiceMaker.py
+++ DFS/DuduServiceMaker.py
@@ -3,7 +3,17 @@ __author__ = 'USER'
 from collections import deque
 
 #Link: https://www.urionlinejudge.com.br/judge/en/problems/view/1610
+def IsParentInList(Node,Grap,Path):
+    FoundPath = list(Path)
+    IsFound = False
+    for c in reversed(list(Path)):
+        for cd in list(Grap[c-1]):
+            if(cd==Node and c!=Path[len(Path)-1]):
+                IsFound = True
+                return True,FoundPath
+        FoundPath.pop()
 
+    return False,Path
 
 def Handle(Arg, Arg1):
    StackRun  = []
@@ -11,12 +21,16 @@ def Handle(Arg, Arg1):
    Visited = []
    Graph = []
    N = Arg1[1]
+   Path = []
+   PathTrace = []
    for c in range(0,Arg1[0]):
        Path.append(-1)
        Visited.append(False)
        Graph.append([])
    for c in range(0,N):
-        Graph[int(Arg[c][0])-1].append(int(Arg[c][1]))
+        if(list(Graph[int(Arg[c][0])-1]).count(int(Arg[c][1]))==0):
+         Graph[int(Arg[c][0])-1].append(int(Arg[c][1]))
+   N=len(Graph)
    for cd in range(1,Arg1[0]+1) :
        if(Graph[cd-1]!=[]):
            StackRun.append(cd)
@@ -25,16 +39,24 @@ def Handle(Arg, Arg1):
            while(len(StackRun)>0):
                 PopL = StackRun.pop()
                 ParentNode = int(PopL)
+                IsPaLis = IsParentInList(ParentNode,Graph,PathTrace)
+                IsPa = IsPaLis[0]
+                if(IsPa==True):
+                    PathTrace = IsPaLis[1]
+                PathTrace.append(ParentNode)
                 ListC = list(Graph[int(ParentNode)-1])
                 for Ele in ListC:
                   if(Visited[int(Ele)-1]==False):
                       StackRun.append(Ele)
                       Visited[int(Ele)-1]=True
-                  else:
+                      Path[int(Ele)-1] = ParentNode
+                  if(PathTrace.count(Ele)==1):
                       return "SIM"
            StackRun = []
+           PathTrace =[]
            for c in range(0,Arg1[0]):
                Visited[c] = False
+               Path[c] =-1
    return  "NAO"
 
 if __name__ == '__main__':
---
+++ Dijkstras/Travellingcost.py
@@ -0,0 +1,12 @@
+__author__ = 'USER'
+try:
+    import Queue as Q  # ver. < 3.0
+except ImportError:
+    import queue as Q
+
+q = Q.PriorityQueue()
+q.put(10)
+q.put(1)
+q.put(5)
+while not q.empty():
+	print q.get(),
