cb91bdb4ad65de4cfc1172715d77ebcb87e8c5e5
binhle <chibinh8@gmail.com>
2017-08-13 23:13:00 +0700

    Binary Search

--- BinarySearch/Pizzamania.py
+++ BinarySearch/Pizzamania.py
@@ -1,32 +1,29 @@
 __author__ = 'Binh Le'
 import sys
 
-def BinarySearch(LMarble,lo,hi,Target,Lpos):
-    if lo <= hi:
+def BinarySearch(LMarble,lo,hi,Target):
+    while lo <= hi:
               mid = int(lo + (hi-lo)/2)
               if ((LMarble[mid] == Target)):
                  return mid
               elif LMarble[mid] < Target:
-                 return BinarySearch(LMarble,mid+1,hi,Target,Lpos)
+                 lo = mid+1
               else:
-                 return BinarySearch(LMarble,lo,mid-1,Target,Lpos)
+                 hi = mid -1
     return -1
 if __name__ == '__main__':
     LPri =[]
     cnt = 0
-    Lpos = []
-    NumTC = int(sys.stdin.readline().strip().split()[0])
+    NumTC = int(sys.stdin.readline().rstrip().split()[0])
     for tc in range(0,NumTC):
         cnt = 0
-        N,PizaPrice = sys.stdin.readline().strip().split()
+        N,PizaPrice = map(int,sys.stdin.readline().rstrip().split())
         N, PizaPrice = int(N),int(PizaPrice)
-        LPri = [int(x) for x in sys.stdin.readline().strip().split()]
+        LPri = [int(x) for x in map(int,sys.stdin.readline().rstrip().split())]
         LPri.sort()
         for id, c in enumerate(LPri):
-                pos = BinarySearch(LPri,0,len(LPri)-1,PizaPrice-c,Lpos)
-                if(pos!=-1 and Lpos.count(pos)!=0):
-                    cnt +=1
-                    Lpos.append(pos)
-
+            pos = BinarySearch(LPri,id+1 ,len(LPri)-1,PizaPrice-c)
+            if(pos!=-1):
+                cnt +=1
         print(cnt)
     pass
