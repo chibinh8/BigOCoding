2432917946eaff933603154dd386b27921739169
binhle <chibinh8@gmail.com>
2017-08-20 19:49:41 +0700

    BTS: MonkandhisFriends

---
+++ BTS/AndysFirst Dictionary.py
@@ -0,0 +1,245 @@
+__author__ = 'Binh Le'
+import sys
+
+class TreeNode:
+    def __init__(self,key,val,left=None,right=None,parent=None):
+        self.key = key
+        self.payload = val
+        self.leftChild = left
+        self.rightChild = right
+        self.parent = parent
+    def Compa(self, other):
+        LWordCodeS  = list(self.key)
+        LWordCodeOth  = list(other)
+        for id, c in enumerate(LWordCodeS):
+            if((len(LWordCodeOth)>id)and c<LWordCodeOth[id]):
+                return -1
+            if((len(LWordCodeOth)>id)and c>LWordCodeOth[id]):
+                return 1
+        if(len(LWordCodeOth)<len(LWordCodeS)):
+            return 1
+        return 0
+    def hasLeftChild(self):
+        return self.leftChild
+
+    def hasRightChild(self):
+        return self.rightChild
+
+    def isLeftChild(self):
+        return self.parent and self.parent.leftChild == self
+
+    def isRightChild(self):
+        return self.parent and self.parent.rightChild == self
+
+    def isRoot(self):
+        return not self.parent
+
+    def isLeaf(self):
+        return not (self.rightChild or self.leftChild)
+
+    def hasAnyChildren(self):
+        return self.rightChild or self.leftChild
+
+    def hasBothChildren(self):
+        return self.rightChild and self.leftChild
+
+    def replaceNodeData(self,key,value,lc,rc):
+        self.key = key
+        self.payload = value
+        self.leftChild = lc
+        self.rightChild = rc
+        if self.hasLeftChild():
+            self.leftChild.parent = self
+        if self.hasRightChild():
+            self.rightChild.parent = self
+
+
+class BinarySearchTree:
+
+    def __init__(self):
+        self.root = None
+        self.size = 0
+        self.cnt = 0
+    def length(self):
+        return self.size
+
+    def __len__(self):
+        return self.size
+
+    def put(self,key,val):
+        if self.root:
+            self._put(key,val,self.root)
+        else:
+            self.root = TreeNode(key,val)
+        self.size = self.size + 1
+
+    def _put(self,key,val,currentNode):
+        if currentNode.Compa(key)==1:
+            if currentNode.hasLeftChild():
+                   self._put(key,val,currentNode.leftChild)
+            else:
+                   currentNode.leftChild = TreeNode(key,val,parent=currentNode)
+        elif(currentNode.Compa(key)==-1):
+            if currentNode.hasRightChild():
+                   self._put(key,val,currentNode.rightChild)
+            else:
+                   currentNode.rightChild = TreeNode(key,val,parent=currentNode)
+        else:
+            currentNode = TreeNode(key,val,parent=currentNode.parent)
+            self.size -=1
+
+    def __setitem__(self,k,v):
+       self.put(k,v)
+
+    def get(self,key):
+       if self.root:
+           res = self._get(key,self.root)
+           if res:
+                  return res.payload
+           else:
+                  return None
+       else:
+           return None
+
+    def _get(self,key,currentNode):
+       if not currentNode:
+           return None
+       elif currentNode.key == key:
+           return currentNode
+       elif currentNode.Compa(key)==1:
+           return self._get(key,currentNode.leftChild)
+       else:
+           return self._get(key,currentNode.rightChild)
+
+    def __getitem__(self,key):
+       return self.get(key)
+
+    def __contains__(self,key):
+       if self._get(key,self.root):
+           return True
+       else:
+           return False
+
+    def delete(self,key):
+      if self.size > 1:
+         nodeToRemove = self._get(key,self.root)
+         if nodeToRemove:
+             self.remove(nodeToRemove)
+             self.size = self.size-1
+         else:
+             raise KeyError('Error, key not in tree')
+      elif self.size == 1 and self.root.key == key:
+         self.root = None
+         self.size = self.size - 1
+      else:
+         raise KeyError('Error, key not in tree')
+
+    def __delitem__(self,key):
+       self.delete(key)
+
+    def spliceOut(self):
+       if self.isLeaf():
+           if self.isLeftChild():
+                  self.parent.leftChild = None
+           else:
+                  self.parent.rightChild = None
+       elif self.hasAnyChildren():
+           if self.hasLeftChild():
+                  if self.isLeftChild():
+                     self.parent.leftChild = self.leftChild
+                  else:
+                     self.parent.rightChild = self.leftChild
+                  self.leftChild.parent = self.parent
+           else:
+                  if self.isLeftChild():
+                     self.parent.leftChild = self.rightChild
+                  else:
+                     self.parent.rightChild = self.rightChild
+                  self.rightChild.parent = self.parent
+
+
+
+    def findMin(self,Node):
+      current = Node
+      while current.hasLeftChild():
+          current = current.leftChild
+      return current
+
+    def remove(self,currentNode):
+         if currentNode.isLeaf(): #leaf
+           if currentNode == currentNode.parent.leftChild:
+               currentNode.parent.leftChild = None
+           else:
+               currentNode.parent.rightChild = None
+         elif currentNode.hasBothChildren(): #interior
+           succ = currentNode.findSuccessor()
+           succ.spliceOut()
+           currentNode.key = succ.key
+           currentNode.payload = succ.payload
+
+         else: # this node has one child
+           if currentNode.hasLeftChild():
+             if currentNode.isLeftChild():
+                 currentNode.leftChild.parent = currentNode.parent
+                 currentNode.parent.leftChild = currentNode.leftChild
+             elif currentNode.isRightChild():
+                 currentNode.leftChild.parent = currentNode.parent
+                 currentNode.parent.rightChild = currentNode.leftChild
+             else:
+                 currentNode.replaceNodeData(currentNode.leftChild.key,
+                                    currentNode.leftChild.payload,
+                                    currentNode.leftChild.leftChild,
+                                    currentNode.leftChild.rightChild)
+           else:
+             if currentNode.isLeftChild():
+                 currentNode.rightChild.parent = currentNode.parent
+                 currentNode.parent.leftChild = currentNode.rightChild
+             elif currentNode.isRightChild():
+                 currentNode.rightChild.parent = currentNode.parent
+                 currentNode.parent.rightChild = currentNode.rightChild
+             else:
+                 currentNode.replaceNodeData(currentNode.rightChild.key,
+                                    currentNode.rightChild.payload,
+                                    currentNode.rightChild.leftChild,
+                                    currentNode.rightChild.rightChild)
+    def FindMax(self,CurrNode):
+      current = CurrNode
+      while current.hasRightChild():
+          current = current.rightChild
+      return current
+    def OutWord(self,CurrNode):
+        if self.root:
+            print(CurrNode.payload)
+            if(CurrNode!=self.root):
+                print(CurrNode.parent.payload)
+            if(CurrNode.hasRightChild()):
+                self.OutWord(self.findMin(CurrNode.rightChild))
+            if(CurrNode.parent!=None and CurrNode.parent.hasRightChild()and CurrNode.parent.rightChild!=CurrNode):
+                self.OutWord(self.findMin(CurrNode.parent.rightChild))
+def EncodeWord(Word):
+    LAscii = []
+    for c in Word:
+        if(c.isupper()):
+            c= c.lower()
+        LAscii.append(ord(c))
+    return LAscii
+def IsAlphaWord(word):
+    for cha in word:
+        if(cha.isalpha()==False):
+            return False
+    return True
+
+if __name__ == '__main__':
+    LWordCode =[]
+    mytree = BinarySearchTree()
+    lines = []
+    while True:
+        line = sys.stdin.readline().rstrip().split()
+        if line:
+            for word in line:
+                if(IsAlphaWord(word))==True:
+                    mytree.put(EncodeWord(word),word)
+        else:
+            break
+    mytree.OutWord(mytree.findMin(mytree.root))
+    pass
